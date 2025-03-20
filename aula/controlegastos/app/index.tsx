0a28cc4ea7fad1bcadd105583f0a8cffe68774ce
Edson Sensato <esensato@hotmail.com>
2025-03-19 22:09:52 -0300

    19/03/2025

--- aula/controlegastos/app/index.tsx
+++ aula/controlegastos/app/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View, KeyboardAvoidingView, FlatList, Image } from "react-native";
 
 export default function Index() {
 
@@ -30,29 +30,81 @@ export default function Index() {
     console.log(destaque);
   }
 
-  return <View style={estilos.painel}>
-    <Text style={estilos.linha}>{getMensagem()} {mensagem} {mensagem} {x * 2}</Text>
-    <Text style={destaque}>Linha 2</Text>
-    <Text style={estilos.linha}>Total: {total}</Text>
-    <View style={{ flexDirection: "row" }}>
-      <Button title="Somar" onPress={somar} />
-      <Button title="Zerar" onPress={zerar} />
-      <Button title="Destaque" onPress={aplicaDestaque} />
+
+  // let listaGasto = ["Item 1", "Item 2", "Item 3", "Item 4"];
+  let [listaGasto, addGasto] = useState(["Item 1", "Item 2", "Item 3", "Item 4"]);
+  let [descricaoGasto, setDescricaoGasto] = useState("");
+
+  return <KeyboardAvoidingView style={estilos.painel}>
+
+    <View style={estilos.cadastro}>
+      <View>
+        <TextInput style={estilos.caixatexto}
+          placeholder="Descrição do Gasto"
+          onChangeText={(conteudo) => setDescricaoGasto(conteudo)}
+          value={descricaoGasto}
+        />
+      </View>
+      <View>
+        <Button title="Incluir Gasto" onPress={() => {
+          addGasto([...listaGasto, descricaoGasto]);
+          setDescricaoGasto("");
+        }} />
+      </View>
     </View>
 
-  </View>
+    <View style={estilos.lista}>
+      <FlatList data={listaGasto}
+        keyExtractor={idx => idx}
+        renderItem={({ item, index }) => {
+          // caso seja par
+          return <View style={estilos.itemLista}>
+            <Image style={{ padding: 5 }} source={require('../assets/images/coin.png')} />
+            <Text style={{ marginLeft: 10 }} key={index}>{index}-{item}</Text>
+          </View>
+        }} />
+    </View>
+
+  </KeyboardAvoidingView >
 
 }
 
 const estilos = StyleSheet.create({
   painel: {
     margin: 50,
-    borderWidth: 2,
     flex: 1
+  },
+  cadastro: {
+    flex: 1,
+    justifyContent: "space-between"
+  },
+  lista: {
+    marginTop: 20,
+    flex: 6
+  },
+  caixatexto: {
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 5
   },
   corpo: {
     flex: 5,
     borderWidth: 2
+  },
+  itemLista: {
+    flexDirection: "row",
+    padding: 10,
+    backgroundColor: "yellow",
+    marginBottom: 10,
+    borderRadius: 5,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
   },
   linha: {
     flex: 1,
